3af9fc7cf6afbe7a584ad3dff4931fe4ac7f6655
Bhavesh <bhaveshahir1210@gmail.com>
2026-06-26 16:10:27 +0530

    Fix : home page !

--- app/[username]/page.js
+++ app/[username]/page.js
@@ -1,33 +1,31 @@
-import React from 'react'
-import PaymentPage from '@/components/PaymentPage'
-import { notFound } from "next/navigation"
-import connectDb from '@/db/connectDb'
-import User from '@/models/User'
-const Username = async ({ params }) => {
+import React from "react";
+import PaymentPage from "@/components/PaymentPage";
+import { notFound } from "next/navigation";
+import connectDb from "@/db/connectDb";
+import User from "@/models/User";
 
+const Username = async ({ params }) => {
   // If the username is not present in the database, show a 404 page
   const checkUser = async () => {
-    await connectDb()
-    let u = await User.findOne({ username: params.username })
+    await connectDb();
+    let u = await User.findOne({ username: params.username });
     if (!u) {
-      return notFound()
+      return notFound();
     }
-  }
-  await checkUser()
-
-
+  };
+  await checkUser();
 
   return (
     <>
       <PaymentPage username={params.username} />
     </>
-  )
-}
+  );
+};
 
-export default Username
- 
+export default Username;
+
 export async function generateMetadata({ params }) {
   return {
     title: `Support ${params.username} - Get Me A Chai`,
-  }
+  };
 }
